f1960e340ff23d912232d011780b44a3e62c6ece
Elijah Wines <wines.elijah@gmail.com>
2023-05-22 19:22:52 -0400

    api is successfully working but no data has yet been added

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
 
-    root 'recipes#index'
+    root 'api/v1/recipes#index'
 
     namespace :api do
       namespace :v1 do
@@ -12,5 +12,5 @@ Rails.application.routes.draw do
         resources only: [:create, :destroy]
       end
     end
-    
+
 end
