e1d99d01a6215e1802e07a58f215a00a6e484add
Elijah Wines <wines.elijah@gmail.com>
2023-05-22 18:55:37 -0400

    ran 'rails g model Recipe name ingredients image_url'

---
+++ db/migrate/20230522224904_create_recipes.rb
@@ -0,0 +1,11 @@
+class CreateRecipes < ActiveRecord::Migration[7.0]
+  def change
+    create_table :recipes do |t|
+      t.string :name
+      t.string :ingredients
+      t.string :image_url
+
+      t.timestamps
+    end
+  end
+end
