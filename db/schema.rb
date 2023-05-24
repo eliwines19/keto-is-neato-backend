4472376aa71f730426ae9d712631bfbd7d5a1f98
Elijah Wines <wines.elijah@gmail.com>
2023-05-24 19:45:23 -0400

    ran 'rails g resources User username password_digest' and added bcrypt method and validations to User model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_22_224904) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_24_232809) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -18,6 +18,13 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_22_224904) do
     t.string "name"
     t.string "ingredients"
     t.string "image_url"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
