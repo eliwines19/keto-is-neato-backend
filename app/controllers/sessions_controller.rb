6093db9ba8cf7bf3c49128614ec521a9c2a4b4b7
Elijah Wines <wines.elijah@gmail.com>
2023-06-20 18:33:18 -0400

    created helper methods for controllers

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,5 @@
+class SessionsControllers < ApplicationController
+
+    
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,4 @@
 Rails.application.routes.draw do
-  resources :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
 
     root 'api/v1/recipes#index'
 
@@ -17,7 +12,6 @@ Rails.application.routes.draw do
     end
 
     resources :users, only: [:create, :show]
-    get "/signup", to: "users#new"
     get "/login", to: "sessions#new"
     post "/sessions", to: "sessions#create"
     delete "/sessions", to: "sessions#destroy"
