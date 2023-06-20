09b13c550db6e238872122cc15edc8a30347b6f5
Elijah Wines <wines.elijah@gmail.com>
2023-06-20 18:39:08 -0400

    added session methods

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,23 @@
 class SessionsControllers < ApplicationController
 
-    
+    def home
+        @user = current_user
+    end
+
+    def create
+        user = User.find_by(name: params[:user][:name])
+        if user && user.authenticate(params[:user][:password])
+            session[:user_id] = user.id
+            # need to redirect to recipes create page
+        else
+            flash[:message] = "Incorrect login information, please try again."
+            # needs to redirect to login page
+        end
+    end
+
+    def destroy
+        session.clear
+        # need to redirect to login page
+    end
 
 end
